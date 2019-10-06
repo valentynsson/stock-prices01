b5c5f2841c9314e46911370712356963b932419f
valentynsson <valentynsson@gmail.com>
2019-10-06 18:13:28 +0000

    filtering lists

--- stock-prices.py
+++ stock-prices.py
@@ -1,14 +1,10 @@
 
 
-
-
-
 def get_data_list(file_data):
     
-
     adj_close = 0
     volume = 0
-
+    big_data = []
     for words in file_data:
     
         words = words.strip()
@@ -18,7 +14,14 @@ def get_data_list(file_data):
         volume = words.index("Volume")
         break
     
-   
+    for line_by_line in file_data:
+        
+        line_by_line = line_by_line.strip()
+        line_by_line = line_by_line.split(",")
+        big_data.append(line_by_line)
+    
+    for i in big_data:
+        print(i)
     return 0
 
 
