2a9e260e46c7c02523d9d4fff6d759dc389f9d02
valentynsson <valentynsson@gmail.com>
2019-10-06 23:07:44 +0000

    validate

--- stock-prices.py
+++ stock-prices.py
@@ -4,6 +4,7 @@ def get_data_list(file_data):
     
     adj_close = 0
     volume = 0
+
     big_data = []
     for words in file_data:
     
@@ -14,15 +15,32 @@ def get_data_list(file_data):
         volume = words.index("Volume")
         break
     
+    
     for line_by_line in file_data:
         
         line_by_line = line_by_line.strip()
         line_by_line = line_by_line.split(",")
         big_data.append(line_by_line)
     
+    
+    check_date = []
+    date_list = []
+    current_date = ""
     for i in big_data:
-        print(i)
+        current_date = i[0][0:7]
+        
+        for c in big_data:
+            if current_date == c[0][0:7]:
+                check_date.append(c)
+            
+        date_list.append(check_date)
+        break  
+            
+    for k in date_list:
+        print(k)    
+    
     return 0
+
 
 
 
