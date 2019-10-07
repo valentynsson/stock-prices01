080bdf5e87b159dddd638d2ac370a810e45eec61
valentynsson <valentynsson@gmail.com>
2019-10-07 00:22:23 +0000

    sorting

--- stock-prices.py
+++ stock-prices.py
@@ -1,5 +1,3 @@
-
-
 def get_data_list(file_data):
     
     adj_close = 0
@@ -15,32 +13,13 @@ def get_data_list(file_data):
         volume = words.index("Volume")
         break
     
-    
     for line_by_line in file_data:
         
         line_by_line = line_by_line.strip()
         line_by_line = line_by_line.split(",")
         big_data.append(line_by_line)
     
-    
-    check_date = []
-    date_list = []
-    current_date = ""
-    for i in big_data:
-        current_date = i[0][0:7]
-        
-        for c in big_data:
-            if current_date == c[0][0:7]:
-                check_date.append(c)
-            
-        date_list.append(check_date)
-        break  
-            
-    for k in date_list:
-        print(k)    
-    
     return 0
-
 
 
 
